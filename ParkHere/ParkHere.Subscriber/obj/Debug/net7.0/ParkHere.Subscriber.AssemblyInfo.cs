e0c8e79f473780f685d78df8bf51596e836c276f
benjamin-cero <benjamin.cero25@gmail.com>
2026-01-05 23:52:41 +0100

    Remove EF Core migration and model snapshot files
    Deleted the initial Entity Framework Core migration and model snapshot files from ParkHere.Services/Migrations. This may indicate a reset or restructuring of the database migrations.

--- ParkHere/ParkHere.Subscriber/obj/Debug/net7.0/ParkHere.Subscriber.AssemblyInfo.cs
+++ ParkHere/ParkHere.Subscriber/obj/Debug/net7.0/ParkHere.Subscriber.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ParkHere.Subscriber")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2464e05536c83b75bc34a2aa94f096e8e8320d87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+784d7cd886901a8392b18dfad414f150abe510d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ParkHere.Subscriber")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ParkHere.Subscriber")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
