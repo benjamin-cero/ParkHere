4c9bfc50690578fe87a6214c6cd408631e5be078
benjamin-cero <benjamin.cero25@gmail.com>
2026-01-06 19:02:46 +0100

    brisanje migracija

--- ParkHere/ParkHere.Services/obj/Debug/net8.0/ParkHere.Services.AssemblyInfo.cs
+++ ParkHere/ParkHere.Services/obj/Debug/net8.0/ParkHere.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ParkHere.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43d6814b58383b2c903ab786d319a663840779b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+382d3698221e5a5cb8a2e5b6372b72a66e5c7a55")]
 [assembly: System.Reflection.AssemblyProductAttribute("ParkHere.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ParkHere.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
